ccf997c59d84972f1f213a8b34aa6a081cdefa70
mms0316 <lurkingdeeply@gmail.com>
2025-04-26 14:30:06 -0300

    Remove unneeded input handling

--- src/main/java/io/github/mms0316/litematica_fetch_addon/event/InputHandler.java
+++ src/main/java/io/github/mms0316/litematica_fetch_addon/event/InputHandler.java
@@ -3,12 +3,10 @@ package io.github.mms0316.litematica_fetch_addon.event;
 import fi.dy.masa.malilib.hotkeys.IHotkey;
 import fi.dy.masa.malilib.hotkeys.IKeybindManager;
 import fi.dy.masa.malilib.hotkeys.IKeybindProvider;
-import fi.dy.masa.malilib.hotkeys.IKeyboardInputHandler;
-import fi.dy.masa.malilib.hotkeys.IMouseInputHandler;
 import io.github.mms0316.litematica_fetch_addon.Reference;
 import io.github.mms0316.litematica_fetch_addon.config.Hotkeys;
 
-public class InputHandler implements IKeybindProvider, IKeyboardInputHandler, IMouseInputHandler
+public class InputHandler implements IKeybindProvider
 {
     private static final InputHandler INSTANCE = new InputHandler();
 
@@ -34,28 +32,5 @@ public class InputHandler implements IKeybindProvider, IKeyboardInputHandler, IM
     public void addHotkeys(IKeybindManager manager)
     {
         manager.addHotkeysForCategory(Reference.MOD_NAME, Reference.MOD_ID+".hotkeys.category.hotkeys", Hotkeys.HOTKEY_LIST);
-    }
-
-    @Override
-    public boolean onKeyInput(int keyCode, int scanCode, int modifiers, boolean eventKeyState)
-    {
-        return false;
-    }
-
-    @Override
-    public boolean onMouseScroll(int mouseX, int mouseY, double amount)
-    {
-        return false;
-    }
-
-    @Override
-    public boolean onMouseClick(int mouseX, int mouseY, int eventButton, boolean eventButtonState)
-    {
-        return false;
-    }
-
-    @Override
-    public void onMouseMove(int mouseX, int mouseY)
-    {
     }
 }
